4a37cd5d809008012764b68bc7ce7bcaaff4e19d
Terenin.iva <terenin1995@gmail.com>
2023-06-07 15:22:11 +0700

    add jankins with remote

--- src/test/java/demoqa/BaseTest.java
+++ src/test/java/demoqa/BaseTest.java
@@ -4,9 +4,13 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import net.bytebuddy.ClassFileVersion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BaseTest {
 
@@ -18,6 +22,19 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
+
+
+        //ссылка на удаленный selenoid, чтобы запуская через ide использовать его
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        //
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//
+//        Configuration.browserCapabilities = capabilities;
 
     }
 
